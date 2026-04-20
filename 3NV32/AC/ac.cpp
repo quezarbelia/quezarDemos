ea51b0f42a0c59b3f9258bf6139dd51180e1843c
quezarbelia <gabrielqva.10@gmail.com>
2026-04-20 14:52:11 -0600

    Add digital invitation and event page templates
    - Created invitacion.html for Bety & Arturo's wedding invitation with sections for music player, countdown timer, event details, and RSVP form.
    - Developed pagina.html for Orenza Eventos featuring a hero section, login form, pricing plans, and a quote calculator for digital invitations.
    - Implemented responsive design and interactive elements for user engagement.

---
+++ 3NV32/AC/ac.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+void menu(){
+    std::cout << "M E N U" << std::endl;
+    std::cout << "\nQuezada Avila Gabriel" << std::endl;
+    std::cout << "1) Metodo Burbuja" << std::endl;
+    std::cout << "2) Algoritmo de ordenacion por insercion" << std::endl;
+    std::cout << "3) Algoritmo por ordenacion rapida" << std::endl;
+    std::cout << "4) Algoritmo de ordenacion Shell" << std::endl;
+    std::cout << "5) Algoritmo de ordenacion de monticulo" << std::endl;
+}
+
+int main() {
+    menu();
+    return 0;
+}   
